0a6c07f5e5a161441be36d5672df3f2a6010d02f
kurtburak <burakudemy17@gmail.com>
2025-08-07 07:35:09 +0300

    turtle_kare_cizildi

---
+++ tosba_ciziyoruz.py
@@ -0,0 +1,10 @@
+import  turtle
+
+turtle_created=turtle.turtles()
+turtle.bgcolor("blue")
+turtle.title("Turtle_Started")
+turtle.color("green")
+for i in range(4):
+    turtle.forward(90)
+    turtle.left(90)
+turtle.done()
